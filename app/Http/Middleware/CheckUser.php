d866019fbc249af7182e742aedb07b87315c09e5
roberto_santos_yela <roberto_santos_apps1ma1819@cev.com>
2020-01-08 05:48:01 +0100

    middleware_check_user_completed

--- app/Http/Middleware/CheckUser.php
+++ app/Http/Middleware/CheckUser.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use App\Helpers\Token;
+use App\User;
 
 class CheckUser
 {
@@ -16,13 +17,19 @@ class CheckUser
      */
     public function handle($request, Closure $next)
     {
-        $request_token = $reques->header('authorization');
+        $request_token = $request->header('Authorization');
         $token = new Token();
         $decoded_token = $token->decode($request_token);
+        $user = User::where('email', '=', $decoded_token->email)->first();  
+        $request->request->add(['user' => $user]);
 
-        
+        if($user != null)
+        {
+            return $next($request);
 
+        }
 
-        return $next($request);
+        ///FALTA ERROR DE TOKEN/// 
+
     }
 }
